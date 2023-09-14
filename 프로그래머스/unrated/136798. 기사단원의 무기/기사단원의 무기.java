b7f5889350af6fa434f95bbcf9dee5e2b04c9859
gossong <93527063+gossong@users.noreply.github.com>
2023-09-14 20:36:45 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.62 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -2,30 +2,27 @@ class Solution {
     public static int solution(int number, int limit, int power) {
     	int answer = 0;
         
-        int [] divNum = new int[number];
+        int [] count = new int[number];
         
         for(int i=1; i<=number; i++){
-           	int cnt = 0; // 약수 개수
            	// 1부터 n까지 모든 수로 나누어보기 - 시간 초과
 //          for(int j=1; j<=i; j++){
 //              if(i%j==0){
-//                  cnt++;
+//                  count[i-1]++;
 //              }
 //          }
-            // 제곱근까지 확인
+            // 제곱근으로 확인 (sqrt로 확인 가능)
             for(int j=1; j*j<=i; j++){
                 if(i%j==0){
-                   divNum[i-1] += 2;
+                   count[i-1] += 2;
                    if(j*j==i){
-                       divNum[i-1] -= 1;
+                       count[i-1] -= 1;
                    }
                }
             }
-
-               System.out.println(divNum[i-1]);
                
-               if(divNum[i-1]<=limit){
-                   answer += divNum[i-1];
+               if(count[i-1]<=limit){
+                   answer += count[i-1];
                }else{
                    answer += power;
                }
