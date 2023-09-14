d36d2b05d78234f542c000ad9a7f87b8bc118e23
gossong <93527063+gossong@users.noreply.github.com>
2023-09-14 20:32:26 +0900

    Update 기사단원의 무기.java

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -5,15 +5,22 @@ class Solution {
         int [] divNum = new int[number];
         
         for(int i=1; i<=number; i++){
-           	int cnt = 0; // 약수
-               for(int j=1; j*j<=i; j++){
-                   if(i%j==0){
-                       cnt += 2;
-                       if(j*j==i){
-                           cnt -= 1;
-                       }
+           	int cnt = 0; // 약수 개수
+           	// 1부터 n까지 모든 수로 나누어보기 - 시간 초과
+//          for(int j=1; j<=i; j++){
+//              if(i%j==0){
+//                  cnt++;
+//              }
+//          }
+            // 제곱근으로 확인 (sqrt로 확인 가능)
+            for(int j=1; j*j<=i; j++){
+                if(i%j==0){
+                   cnt += 2;
+                   if(j*j==i){
+                       cnt -= 1;
                    }
                }
+            }
                
                divNum[i-1] = cnt;
                System.out.println(divNum[i-1]);
