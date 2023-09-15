12ca2896d8b67efef99947264886e319c55df99f
gossong <93527063+gossong@users.noreply.github.com>
2023-09-15 12:32:34 +0900

    Update 최소직사각형.java

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int w = 0; // 가로
-        int h = 0; // 세로
+        int w = 0;
+        int h = 0;
         
         for(int i=0; i < sizes.length; i++){
+            // 작은 수를 1열(w), 큰 수를 2열(h)에 저장
         	if(sizes[i][0]>sizes[i][1]) {
         		int c = sizes[i][0];
         		sizes[i][0] = sizes[i][1];
@@ -19,5 +20,17 @@ class Solution {
         }
         
         return w * h;
+    }
+
+    // Math.max, Math.min을 사용
+    public int solution(int[][] sizes) {
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            // 0행, 1행 중 - length : 큰 값 저장, height : 작은 값 저장
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
+        }
+        int answer = length * height;
+        return answer;
     }
 }
