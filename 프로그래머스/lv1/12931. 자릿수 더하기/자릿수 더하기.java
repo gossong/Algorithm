e2942bd80dcf09717e015236481bac39a6c56c31
gossong <93527063+gossong@users.noreply.github.com>
2023-09-17 14:59:45 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -9,6 +9,16 @@ public class Solution {
             n = n%((int)Math.pow(10,i));
         }
 
+        // 끝자리 부터 더하기
+        // while(true){
+        //     answer+=n%10;
+        //     if(n<10)
+        //         break;
+
+        //     n=n/10;
+        // }
+
+        
         return answer;
     }
 }
