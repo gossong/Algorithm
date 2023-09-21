56a748428cb168c8c788c826591f26b031beaf34
gossong <93527063+gossong@users.noreply.github.com>
2023-09-21 17:40:12 +0900

    Update 정수를 나선형으로 배치하기.java

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -8,6 +8,7 @@ class Solution {
         int way = 1;  // 방향에 따라 +1, -1
         
         while(n>0){
+            // 행(x) 유지, 열(y) 이동
             for(int i = 0; i < n; i++){
                 y += way;
                 answer[x][y] = num++;
@@ -15,11 +16,12 @@ class Solution {
             
             n--;
             
+            // 열(y) 유지, 행(x) 이동
             for(int i = 0; i < n; i++){
                 x += way;
                 answer[x][y] = num++;
             }
-            
+            // y축, x축 다 돌면 방향 전환
             way *= -1;
             
         } 
