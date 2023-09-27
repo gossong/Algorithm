7ff5ebb26a501de9bdda4088819dbcbb3447b42b
gossong <93527063+gossong@users.noreply.github.com>
2023-09-27 14:42:31 +0900

    Update 안전지대.java

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -5,9 +5,11 @@ class Solution {
         int n = board.length;
         int[][] check = new int[n][n];
 
+        // 지뢰 찾기
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(board[i][j]==1){
+                    // 지뢰의 인접한 칸(+1, -1 칸) check(+1) 하기
                     for(int k = i-1; k < i+2; k++){
                         for(int l = j-1; l < j+2; l++){
                             if(k>=0&&l>=0&&k<n&&l<n){
@@ -18,6 +20,7 @@ class Solution {
                 }         
             }
         }
+        // check되지 않은 칸 확인하기
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(check[i][j]==0) answer++;
