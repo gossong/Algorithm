63515b806bb50998287af1e9809bdc0ba07f3985
gossong <93527063+gossong@users.noreply.github.com>
2023-10-02 19:53:29 +0900

    [level 2] Title: 올바른 괄호, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        // Deque 사용
+        Deque<Character> deque = new ArrayDeque<Character>();
+		
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);;
+			
+			// 여는 괄호
+			if(c == '(') {
+                deque.add(c);
+				
+			// 닫는 괄호
+			}else {
+                // deque이 비어있지 않으면서, 이전 '('일 경우 -> () 제거
+                if(deque.size()>0 && deque.getLast() == '(') {
+                    deque.remove();
+                }else {
+                    deque.add(c);
+                }	
+			}
+		}
+        
+        // deque이 비어있을 경우, true
+        if(deque.size()==0){
+            answer = true;
+        }
+        
+        
+        return answer;
+    }
+}
