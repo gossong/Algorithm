cdbea5699989c4b5e79025ee196af8f9bbd74826
gossong <93527063+gossong@users.noreply.github.com>
2023-10-26 09:11:14 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -17,5 +17,14 @@ class Solution {
             }
         }
         return answer;
+
+
+        // String answer = "0";
+
+        // for (int i = food.length - 1; i > 0; i--) {
+        //     for (int j = 0; j < food[i] / 2; j++) {
+        //         answer = i + answer + i; 
+        //     }
+        // }
     }
 }
