3e75fd08fb0845f094764818ea26328ae73dce66
gossong <93527063+gossong@users.noreply.github.com>
2023-10-31 20:32:58 +0900

    [level 2] Title: 구명보트, Time: 8.47 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        
+        Arrays.sort(people);  // 오름차순으로 정렬 50 50 70 80
+
+        int check = 0;
+        int j = 0;
+        for(int i = people.length-1; i >= j; i--){
+            if(people[i]+people[j]<=limit){
+                j++;
+            }
+            check++;
+        }
+        
+        return check;
+    }
+}
