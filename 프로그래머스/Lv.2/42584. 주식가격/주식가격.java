501bf5828d2ea18133dc4cd2049e841fa9023f6f
gossong <93527063+gossong@users.noreply.github.com>
2024-01-15 18:29:40 +0900

    Update 주식가격.java

--- 프로그래머스/Lv.2/42584. 주식가격/주식가격.java
+++ 프로그래머스/Lv.2/42584. 주식가격/주식가격.java
@@ -2,14 +2,16 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
 
+	    	// O(n^2)
 		for (int i = 0; i < prices.length; i++) {
 			for (int j = i + 1; j < prices.length; j++) {
-				answer[i]++; // 뒤에 있는 값들 보다 작거나 같을 때 인덱스 값 하나씩 추가
-				if (prices[i] > prices[j]) { // 크면 다음 인데스 비교
+				answer[i]++; 
+				if (prices[i] > prices[j]) {
 					break;
 				}
 			}
 		}
+	    	
 		return answer;
     }
 }
