5c50490d3963e364c0877572ec6997d954328672
gossong <93527063+gossong@users.noreply.github.com>
2024-01-19 12:40:18 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.69 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = k; i <= n; i+=k){
+            list.add(i);
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i).intValue();
+        }
+        return answer;
+    }
+}
