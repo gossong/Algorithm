799b26a3e34d4dacac67ee2cfbaa6a6a1e9808c7
gossong <93527063+gossong@users.noreply.github.com>
2024-02-07 16:20:57 +0900

    [level 0] Title: 외계행성의 나이, Time: 7.26 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String ageString = "" + age;
+        for(int i = 0; i < ageString.length(); i++){
+            String ageSub = ageString.substring(i, i+1);
+            switch(ageSub){
+                case "0" : answer += "a"; break;
+                case "1" : answer += "b"; break;
+                case "2" : answer += "c"; break;
+                case "3" : answer += "d"; break;
+                case "4" : answer += "e"; break;
+                case "5" : answer += "f"; break;
+                case "6" : answer += "g"; break;
+                case "7" : answer += "h"; break;
+                case "8" : answer += "i"; break;
+                case "9" : answer += "j"; break;
+            }
+        }
+        return answer;
+    }
+}
