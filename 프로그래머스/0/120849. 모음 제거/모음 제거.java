ea13ccc2d250bca0aee9a8f1c0b826f0a1dd74e4
gossong <93527063+gossong@users.noreply.github.com>
2024-02-13 14:34:33 +0900

    [level 0] Title: 모음 제거, Time: 0.14 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,18 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] vowel = {"a","e","i","o","u"};
-        for(int s = 0; s < my_string.length(); s++){
-            boolean check = true;
-            for(int i = 0; i < 5; i++){
-                if(my_string.substring(s,s+1).equals(vowel[i])){
-                    check = false;        
-                }
-            }
-            if(check){
-                answer += my_string.substring(s,s+1);
-            }
-        }
+        answer = my_string.replaceAll("[aeiou]", "");
         return answer;
     }
 }
