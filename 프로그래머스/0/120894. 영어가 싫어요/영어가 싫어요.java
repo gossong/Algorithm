291e627d53ac5c94d76a93d6f6209a0b89c34e79
gossong <93527063+gossong@users.noreply.github.com>
2024-02-16 16:26:40 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.39 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,11 +1,10 @@
 class Solution {
     public long solution(String numbers) {
         String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        String string_num = numbers;
         for(int i = 0; i < number.length; i++){
-            string_num = string_num.replaceAll(number[i] ,i + "");
+            numbers = numbers.replaceAll(number[i],String.valueOf(i));
         } 
-        long answer = Long.parseLong(string_num);
+        long answer = Long.parseLong(numbers);
         return answer;
     }
 }
