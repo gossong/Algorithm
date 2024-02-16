44645b415337a906fa8c22c662a8cc90530114aa
gossong <93527063+gossong@users.noreply.github.com>
2024-02-16 15:59:29 +0900

    [level 0] Title: 369게임, Time: 8.16 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String[] nums = (order+"").split("");
+        for(String num : nums){
+            if(num.equals("3")||num.equals("6")||num.equals("9")){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
