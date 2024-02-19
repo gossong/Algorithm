c0696b9ebaf5af5d625ecd3791167f68fdd6bc24
gossong <93527063+gossong@users.noreply.github.com>
2024-02-19 13:58:50 +0900

    [level 0] Title: 숫자 찾기, Time: 8.05 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num, int k) {
-        String str = num + "";
-        int answer = str.indexOf(k+"") == -1 ? -1 : str.indexOf(k+"") + 1;
+        String str = String.valueOf(num);
+        int answer = str.indexOf(String.valueOf(k)) == -1 ? -1 : str.indexOf(k+"") + 1;
         return answer;
     }
 }
