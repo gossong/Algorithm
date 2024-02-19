e3deb923ccc0e8d147169c8ef342bf5697fab716
gossong <93527063+gossong@users.noreply.github.com>
2024-02-19 14:04:20 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.14 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for(String s : str.split("")){
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
