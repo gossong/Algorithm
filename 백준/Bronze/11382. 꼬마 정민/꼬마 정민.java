1e11b3654924a0e473d25f9bad956c1557c0edc6
gossong <93527063+gossong@users.noreply.github.com>
2024-02-21 11:37:52 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+    	Scanner scanner = new Scanner(System.in);
+        Long A = scanner.nextLong();
+        Long B = scanner.nextLong();
+        Long C = scanner.nextLong();
+
+        System.out.println(A+B+C);
+    }
+}
