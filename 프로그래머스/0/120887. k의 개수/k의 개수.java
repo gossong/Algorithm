d774d63e0e08067bdeeb1700c23d7a8579aa2288
gossong <93527063+gossong@users.noreply.github.com>
2024-02-25 18:13:42 +0900

    [level 0] Title: k의 개수, Time: 3.33 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,14 +1,23 @@
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        for(int num = i; num <= j; num++){
-            String number = String.valueOf(num);
-            if(number.contains(String.valueOf(k))){
-                for(int l = 0; l < number.length(); l++){
-                    if(number.substring(l,l+1).equals(String.valueOf(k))){
-                        answer++;
-                    }
+        // for(int num = i; num <= j; num++){
+        //     String number = String.valueOf(num);
+        //     if(number.contains(String.valueOf(k))){
+        //         for(int l = 0; l < number.length(); l++){
+        //             if(number.substring(l,l+1).equals(String.valueOf(k))){
+        //                 answer++;
+        //             }
+        //         }
+        //     }
+        // }
+        for(int n = i; n <= j; n++){
+            int num = n;
+            while(num != 0){
+                if(num % 10 == k){
+                    answer++;
                 }
+                num /= 10;
             }
         }
         return answer;
