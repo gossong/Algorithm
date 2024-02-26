2d0013a95fb3f4e06bcbf678df46a70bab7e32a2
gossong <93527063+gossong@users.noreply.github.com>
2024-02-26 17:10:21 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(int M, int N) {
-        int answer = 0;
-        for(int i = 1; i < M; i++){
-            answer++;
-        }
-        for(int i = 1; i < N; i++){
-            answer+=M;
-        }
+        int answer = M*N-1;
         return answer;
     }
 }
