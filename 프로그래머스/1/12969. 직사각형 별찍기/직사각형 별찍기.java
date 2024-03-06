31989bbe04b993b6ecb296b2f736f7fefb9453f3
gossong <93527063+gossong@users.noreply.github.com>
2024-03-06 10:19:16 +0900

    [level 1] Title: 직사각형 별찍기, Time: 234.30 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while(b>0){
+            System.out.println("*".repeat(a));
+            b--;
+        }
+    }
+}
