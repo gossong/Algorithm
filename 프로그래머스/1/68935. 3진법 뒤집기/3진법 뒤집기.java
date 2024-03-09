090846d4513ad5108e6a98150966e8db312e9f52
gossong <93527063+gossong@users.noreply.github.com>
2024-03-09 11:34:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.93 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int n) {
         String str = "";
-        StringBuilder sb = new StringBuilder();
         while(n!=0){
-            str = n%3 + str;
+            str = str + n%3;
             n /= 3;
         }
-        sb.append(str);
-        sb.reverse();
-        int answer = Integer.parseInt(sb.toString(), 3);
+        int answer = Integer.parseInt(str, 3);
         
         return answer;
     }
